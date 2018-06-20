b2b3b4d39f8106bc8368f193cd48a6a16cdfefbb
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 21:41:02 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+      i = 0
+      newarray = []
+      while i < collection.length
+            newarray << ield(collection[i])
+            i = i + 1
+      end
+      newarray
 end
